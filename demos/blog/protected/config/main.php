a421d87ff691619b75b53fbae5bde41e0af95ad5
Ilya Gulevsky <polanski10@mail.ru>
2011-10-26 18:44:57 +0400

    after work

--- demos/blog/protected/config/main.php
+++ demos/blog/protected/config/main.php
@@ -8,6 +8,7 @@
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'Yii Blog Demo',
+        //'theme'=>'themename',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -21,54 +22,71 @@ return array(
 
 	'defaultController'=>'post',
 
+        'modules'=>array(
+            'gii'=>array(
+                'class'=>'system.gii.GiiModule',
+                'password'=>'123456',
+                // 'ipFilters'=>array(…список IP…),
+                // 'newFileMode'=>0666,
+                // 'newDirMode'=>0777,
+            ),
+        ),
+
 	// application components
 	'components'=>array(
-		'user'=>array(
-			// enable cookie-based authentication
-			'allowAutoLogin'=>true,
-		),
-		
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=blog',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '123456',
-			'charset' => 'utf8',
-			'tablePrefix' => 'tbl_',
-		),
-		
-		'errorHandler'=>array(
-			// use 'site/error' action to display errors
-            'errorAction'=>'site/error',
-        ),
-        'urlManager'=>array(
-            'urlFormat'=>'path',
-            'rules'=>array(
-                'post/<id:\d+>/<title:.*?>'=>'post/view',
-                'posts/<tag:.*?>'=>'post/index',
-                '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-            ),
-        ),
-        'log'=>array(
-            'class'=>'CLogRouter',
-            'routes'=>array(
-                array(
-                        'class'=>'CFileLogRoute',
-                        'levels'=>'error, warning',
-                ),
-                // uncomment the following to show log messages on web pages
 
-//                array(
-//                        'class'=>'CWebLogRoute',
-//                ),
-                array( // configuration for the toolbar
-                  'class'=>'XWebDebugRouter',
-                  'config'=>'alignLeft, opaque, runInDebug, fixedPos, collapsed, yamlStyle',
-                  'levels'=>'error, warning, trace, profile, info',
-                  //'allowedIPs'=>array('127.0.0.1','::1','192.168.1.54','192\.168\.1[0-5]\.[0-9]{3}'),
-                 ),
+            'user'=>array(
+                    // enable cookie-based authentication
+                    'allowAutoLogin'=>true,
+            ),
+
+            'db'=>array(
+                    'connectionString' => 'mysql:host=localhost;dbname=blog',
+                    'emulatePrepare' => true,
+                    'username' => 'root',
+                    'password' => '123456',
+                    'charset' => 'utf8',
+                    'tablePrefix' => 'tbl_',
+            ),
+
+            'errorHandler'=>array(
+                    // use 'site/error' action to display errors
+                    'errorAction'=>'site/error',
+            ),
+            /*
+            'urlManager'=>array(
+                'urlFormat'=>'path',
+                //'showScriptName' => false,
+                'rules'=>array(
+                    'post/<id:\d+>/<title:.*?>'=>'post/view',
+                    'posts/<tag:.*?>'=>'post/index',
+                    '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+                    'gii'=>'gii',
+                    'gii/<controller:\w+>'=>'gii/<controller>',
+                    'gii/<controller:\w+>/<action:\w+>'=>'gii/<controller>/<action>',
                 ),
             ),
+            */
+            'log'=>array(
+                'class'=>'CLogRouter',
+                'routes'=>array(
+                    array(
+                            'class'=>'CFileLogRoute',
+                            'levels'=>'error, warning',
+                    ),
+                    // uncomment the following to show log messages on web pages
+
+    //                array(
+    //                        'class'=>'CWebLogRoute',
+    //                ),
+                    array( // configuration for the toolbar
+                      'class'=>'XWebDebugRouter',
+                      'config'=>'alignLeft, opaque, runInDebug, fixedPos, collapsed, yamlStyle',
+                      'levels'=>'error, warning, trace, profile, info',
+                      //'allowedIPs'=>array('127.0.0.1','::1','192.168.1.54','192\.168\.1[0-5]\.[0-9]{3}'),
+                     ),
+                    ),
+                ),
 	),
 
 	// application-level parameters that can be accessed
